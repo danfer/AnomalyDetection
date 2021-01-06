986bc676c409fcd7915a6815c42bcad79c9b914b
Danfer <dhabed@outlook.com>
2021-01-06 05:58:03 -0600

    Trainer and program

--- NetworkTraffic/ML/Trainer.cs
+++ NetworkTraffic/ML/Trainer.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Microsoft.ML;
+using NetworkTraffic.ML.Base;
+using NetworkTraffic.ML.Objects;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace NetworkTraffic.ML
 {
-    class Trainer
+    public class Trainer: BaseML
     {
+        private const int PvalueHistoryLength = 3;
+        private const int SeasonalityWindowSize = 3;
+        private const int TrainingWindowSize = 7;
+        private const int Confidence = 98;
+        public void Train(string trainingFileName)
+        {
+            if (!File.Exists(trainingFileName))
+            {
+                Console.WriteLine($"Failed to find training data file ({trainingFileName}");
+
+                return;
+            }
+            var trainingDataView = GetDataView(trainingFileName);
+
+            var trainingPipeLine = MlContext.Transforms.DetectSpikeBySsa(
+                nameof(NetworkTrafficPrediction.Prediction),
+                nameof(NetworkTrafficHistory.BytesTransferred),
+                confidence: Confidence,
+                pvalueHistoryLength: PvalueHistoryLength,
+                trainingWindowSize: TrainingWindowSize,
+                seasonalityWindowSize: SeasonalityWindowSize);
+
+            ITransformer trainedModel = trainingPipeLine.Fit(trainingDataView);
+
+            MlContext.Model.Save(trainedModel, trainingDataView.Schema, ModelPath);
+
+            Console.WriteLine("Model trained");
+        }
     }
 }
--- NetworkTraffic/Program.cs
+++ NetworkTraffic/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NetworkTraffic.ML;
+using System;
 
 namespace NetworkTraffic
 {
@@ -6,7 +7,29 @@ namespace NetworkTraffic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 2)
+            {
+                Console.WriteLine($"Invalid arguments passed in, exiting.{Environment.NewLine}{Environment.NewLine}Usage:{Environment.NewLine}" +
+                                  $"predict <path to input file>{Environment.NewLine}" +
+                                  $"or {Environment.NewLine}" +
+                                  $"train <path to training data file>{Environment.NewLine}");
+
+                return;
+            }
+
+            switch (args[0])
+            {
+                case "predict":
+                    new Predictor().Predict(args[1]);
+                    break;
+                case "train":
+                    new Trainer().Train(args[1]);
+                    break;
+                default:
+                    Console.WriteLine($"{args[0]} is an invalid option");
+                    break;
+            }
+
         }
     }
 }
